de6cfad88c0bce349829f4a6929a31cd79e8e54c
Tom Esparon <tom.esparon@gmail.com>
2017-03-05 19:11:53 +0000

    Added a comments model, however the forenKey not work

--- adventure_quest_project/adventureQuest/models.py
+++ adventure_quest_project/adventureQuest/models.py
@@ -2,8 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-
-
+from datetime import datetime
 
 # Create your models here.
 class UserProfile(models.Model):
@@ -33,3 +32,23 @@ class Riddle(models.Model):
 
     def __unicode__(self):
         return self.riddleTwo
+
+# Model for comments
+class Comment(models.Model):
+
+	#needs a foreign key
+	#onPage = models.ForeignKey('adventureQuest.Riddle', related_name='comments')
+	author = models.CharField(max_length=128)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=datetime.now)
+	approved_comment = models.BooleanField(default=False)
+
+	def approve(self):
+		self.approved_comment = True
+		self.save()
+
+	def __str__(self):
+		return self.text
+
+	def __unicode__(self):
+		return self.text	
