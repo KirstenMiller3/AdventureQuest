572396768429f379c26cfcf663d90b3c2a439e18
unknown <2286107s@BO1028-6-06.ad.dcs.gla.ac.uk>
2017-03-23 14:20:52 +0000

    adding trophy to congrats

--- adventure_quest_project/static/js/adventureQuest-jquery.js
+++ adventure_quest_project/static/js/adventureQuest-jquery.js
@@ -1,87 +1,81 @@
 
-/* Ajax Query */
-
+/* Ajax Riddle Submit Query */
 $('#questionform').submit(function(e) {
+
+	// Prevent form submission
 	e.preventDefault();
-	
+
+	// Create question box and value
 	var questionBox = $('#question');
 	var answerValue = questionBox[0].value;
-	
+
+	// Set up ajax request
 	$.ajax({
 		type: 'GET',
 		url: '/adventureQuest/quest_ajax/',
 		data: {'answer': answerValue},
 		success: function(response) {
+
 			var answerDiv = $('#answer');
 			var answerFromServer = response['answer'];
+
+			// Update question and clear answer
 			answerDiv.text(answerFromServer);
 			questionBox.val('');
 
 			var hintDiv = $('#hint');
 			var hintFromServer = response['hint'];
+			//Update hint text
 			hintDiv.text(hintFromServer);
 
 			var instNoDiv = $('#instruction');
 			var y = response['instruction'];
+			// Update instruction text
 			instNoDiv.text(y);
+
+			var available = response['hint_available'];
+			// Check if a hint is available and if not hide the hint button
+			if (available === 'false'){
+				var button = $('#hintform')
+					button.hide();
+			}
+			else if (available === 'true'){
+				var button = $('#hintform')
+					button.show();
+			}
 
 			var lastQ = response['noRiddles']
 			var currentQ = response['currentQ']
-			console.log("no of Q " + lastQ)
-			console.log("currentQ " + currentQ)
-
-			var blergh = response['correct']
-			// Trying to get the congratulations pop-up to work
-			//var questionNo = response['riddleQuestionID']
-			//var lastQuestion = response['noRiddles']
-
-			var available = response['hint_available'];
-			console.log("hint it from ajax" + available)
-
-			if (available === 'false'){
-				console.log('false')
-				var button = $('#hintform')
-					button.hide();
-
-			}
-			else if (available === 'true'){
-				console.log('true')
-				var button = $('#hintform')
-					button.show();
-
-			}
-
-			if(currentQ === lastQ-1 && blergh)
+			var end = response['correct']
+			// Check if the question is the last one and it was correct. If so
+			// then redirect the user to the last page
+			if(currentQ === lastQ-1 && end)
 			{
-				console.log("ENTERED")
 				$(this).unbind('submit').submit()
-				//location.href = 'adventureQuest/congratulations/'
 				window.location = '../congratulations/'
-					//"http://127.0.0.1:8000/adventureQuest/congratulations/"
-				//location.replace('adventureQuest/congratulations/')
-
 			}
-			//if (questionNo === lastQuestion - 1){
-			//	console.log("!!!!!!!!!!!!!!!!!!!!")
-				//$(this).unbind('submit').submit()
-			//}
 
 		},
 	});
 	
 });
 
+/* Ajax Hint Submit Query */
 $('#hintform').click(function(e) {
-	$('#hintform').click('disabled', false)
+
+	// Prevent form submission
 	e.preventDefault();
 
+	// Set up ajax request
 	$.ajax({
 		type: 'GET',
 		url: '/adventureQuest/quest_ajax/',
 		data: {click: true},
 		success: function(response) {
+
 			var hintDiv = $('#hint');
 			var hintFromServer = response['hint'];
+			// Update the hint text
 			hintDiv.text(hintFromServer);
 
 			var hintNoDiv = $('#hintNo');
